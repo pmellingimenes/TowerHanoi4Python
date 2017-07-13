4c467bf6bb8d440d3544d5e24f07fa41c74b0cab
Paulo Mellin Gimenes <paulo@dlie-015.intranet.dlie.unicamp.br>
2017-07-13 19:41:38 -0300

    Implementing discs movement

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ MG's Tower of Hanoi for Python - Main Module
 """
 import pygame
 import models
- 
+#disc number
+N = 7  
 # Define some colors constants
-N = 3 #disc number
 BLACK = [0, 0, 0]
 WHITE = [255, 255, 255]
 GREEN = [0, 255, 0]
 RED = [255, 0, 0]
 BOARD_COLOR = [153, 76, 0]
+# Define elements size constants
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 700
 BOARD_WIDTH = SCREEN_WIDTH/2
@@ -21,45 +22,46 @@ POS_WIDTH = 20
 POS_HEIGHT = 200
 DISC_WIDTH = 200
 DISC_HEIGHT = POS_WIDTH
+# Init pygame
 pygame.init()
 
 # Define the screen (and it's properties)
-
 size = [SCREEN_WIDTH, SCREEN_HEIGHT]
-
 screen = pygame.display.set_mode(size)
-
 pygame.display.set_caption("MG's Tower of Hanoi for Python")
 
 # Define the game sprites lists
 all_sprites_list = pygame.sprite.Group()
-disc_sprites_list = pygame.sprite.Group()
+pos_sprites_list = pygame.sprite.Group()
+
 # Draw the game board (and it's positions)
 game_board = models.Block(BOARD_COLOR, BOARD_WIDTH,BOARD_HEIGHT)
 game_board.rect.x= BOARD_X
 game_board.rect.y= BOARD_Y
-first_pos = models.Block(BOARD_COLOR, POS_WIDTH,POS_HEIGHT)
+first_pos = models.Position(0,BOARD_COLOR, POS_WIDTH,POS_HEIGHT)
 first_pos.rect.x = BOARD_X 
 first_pos.rect.y=  BOARD_Y - POS_HEIGHT
-second_pos = models.Block(BOARD_COLOR, POS_WIDTH,POS_HEIGHT)
+second_pos = models.Position(1,BOARD_COLOR, POS_WIDTH,POS_HEIGHT)
 second_pos.rect.x = (BOARD_X + (BOARD_WIDTH/2)) - (POS_WIDTH/2)
 second_pos.rect.y= BOARD_Y - POS_HEIGHT
-third_pos = models.Block(BOARD_COLOR,POS_WIDTH,POS_HEIGHT)
+third_pos = models.Position(2,BOARD_COLOR,POS_WIDTH,POS_HEIGHT)
 third_pos.rect.x = (BOARD_X + BOARD_WIDTH) - POS_WIDTH
 third_pos.rect.y= BOARD_Y - POS_HEIGHT
 
 all_pos = [first_pos,second_pos,first_pos,third_pos]
 all_sprites_list.add([game_board,all_pos])
+pos_sprites_list.add(all_pos)
 discs = []
 # Draw the game discs
 for i in range(0,N):
     if i % 2 == 0:
-        disc = models.Block(RED,(DISC_WIDTH/(i+1)),DISC_HEIGHT)
+        disc = models.Disc(0,i,RED,(DISC_WIDTH/(i+1)),DISC_HEIGHT)
     else:
-        disc = models.Block(GREEN,(DISC_WIDTH/(i+1)),DISC_HEIGHT)
+        disc = models.Disc(0,i,GREEN,(DISC_WIDTH/(i+1)),DISC_HEIGHT)
     disc.rect.x = BOARD_X - ((DISC_WIDTH/(i+1)/2)-(DISC_HEIGHT/2))
     disc.rect.y = (BOARD_Y - DISC_HEIGHT) - (DISC_HEIGHT*i)
     discs.append(disc)
+    all_pos[0].discs.append(disc)
 all_sprites_list.add(discs)
 # Discs' move variables
 done = False
@@ -68,11 +70,8 @@ drop = False
 move = False
 disc_index = None
 last_pos = [0,0]
-
 # Manage how fast the screen updates
 clock = pygame.time.Clock()
-score = 0
-
 # -------- Main Game Loop -----------
 while not done:
     # --- Main event loop
@@ -83,17 +82,16 @@ while not done:
             drag = True
             drop = False
             for i in range(0,N):
-                if(discs[i].is_clicked()):
+                if discs[i].is_clicked():
                     disc_index = i
+                    last_pos = [discs[i].rect.x,discs[i].rect.y]
                     move = True
-                
-
+                    break
         elif event.type == pygame.MOUSEBUTTONUP:
             drag = False
             drop = True
-            last_pos = event.pos
+            
     screen.fill(WHITE)
-
     if drag:
         if move:
             pos = pygame.mouse.get_pos()
@@ -101,8 +99,8 @@ while not done:
             discs[disc_index].rect.y = pos[1] - (discs[disc_index].height/2)
     elif drop:
         if move:
-            discs[disc_index].rect.x = last_pos[0] - (discs[disc_index].width/2)
-            discs[disc_index].rect.y = last_pos[1] - (discs[disc_index].height/2)
+            discs[disc_index].rect.x = last_pos[0]
+            discs[disc_index].rect.y = last_pos[1]
             move = False
     all_sprites_list.draw(screen)
     # --- update  screen.
--- models.py
+++ models.py
@@ -9,3 +9,13 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
     def is_clicked(self):
         return pygame.mouse.get_pressed()[0] and self.rect.collidepoint(pygame.mouse.get_pos())
+class Position(Block):
+    def __init__(self, pos_index,color, width, height):
+        Block.__init__(self,color, width, height)
+        self.pos_index = pos_index
+        self.discs = []
+class Disc(Block):
+    def __init__(self,current_pos,size,color, width, height):
+        Block.__init__(self,color, width, height)
+        self.current_pos = current_pos
+        self.size = size
