21a7d2a581f8aedf145623b48eee6701c2a1c2e7
Paulo Mellin Gimenes <pmellingimenes@gmail.com>
2017-07-16 22:26:32 -0300

    Adding coments to models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@
 MG's Tower of Hanoi for Python - Models Module
 """
 import pygame
+# Generic Block class
 class Block(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
@@ -12,16 +13,19 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
     def is_clicked(self):
         return pygame.mouse.get_pressed()[0] and self.rect.collidepoint(pygame.mouse.get_pos())
+# Game positions class
 class Position(Block):
     def __init__(self, pos_index,color, width, height):
         Block.__init__(self,color, width, height)
         self.pos_index = pos_index
         self.discs = []
+# Game discs class
 class Disc(Block):
     def __init__(self,current_pos,id,color, width, height):
         Block.__init__(self,color, width, height)
         self.current_pos = current_pos
         self.id = id
+# Buttons class
 class Button(Block):
     def __init__(self,text,text_color,text_size,text_font,color, width, height):
         Block.__init__(self,color, width, height)
